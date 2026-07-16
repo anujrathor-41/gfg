d1f1a4721f2167ccf8a254989b40a529295c65fd
Anuj Rathor <anujrathor41@gmail.com>
2026-07-16 23:09:17 +0530

    Time: 0 ms (100%), Space: 41.8 MB (37.46%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,69 +1,79 @@
+class Solution {
+public:
+    int minSubArrayLen(int k, vector<int>& arr) {
+        int n=arr.size();
+        int i=0;
+        int j=0;
+        int mn=INT_MAX;
+        int sum=0;
+
+        while(j<n){
+            // cal
+            sum=sum+arr[j];
+            
+
+            if(sum<k){
+                j++;
+            }
+            else if(sum>=k){
+
+                while(sum>=k){
+                    mn=min(mn,j-i+1);
+
+                    sum =sum-arr[i];
+                    i++;
+                }
+                j++;
+                
+            }
+            
+            
+        }
+        if(mn==INT_MAX){
+            return 0;
+        }else {
+            return mn;
+        }
+    }
+};// wrong
+
+
 // class Solution {
 // public:
-//     int minSubArrayLen(int k, vector<int>& arr) {
-//         int n=arr.size();
-//         int i=0;
-//         int j=0;
-//         int mn=INT_MAX;
-//         int sum=0;
+//     int minSubArrayLen(int target, vector<int>& nums) {
 
-//         while(j<n){
-//             // cal
-//             sum=sum+arr[j];
-//             j++;
+//         int n = nums.size();
+//         int i = 0;
+//         int j = 0;
+//         int sum = 0;
+//         int mn = INT_MAX;
 
-//             if(sum<k){
+//         while (j < n) {
+
+//             // calculation
+//             sum += nums[j];
+
+//             if (sum < target) {
 //                 j++;
 //             }
-//             else if(sum==k){
-//                 mn=min(mn,j-i+1);
-//                 j++;
-//             }
-//             else if(sum>k){
-//                 while(sum>k){
-//                     sum=sum-arr[i];
+//             else if (sum >= target) {
+
+//                 while (sum >= target) {
+
+//                     mn = min(mn, j - i + 1);
+
+//                     sum -= nums[i];
 //                     i++;
 //                 }
+
 //                 j++;
 //             }
 //         }
-//         return mn;
+
+//         if(mn==INT_MAX){
+//             return 0;
+//         }else {
+//             return mn;
+//         }
 //     }
 // };
-
-
-class Solution {
-public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-
-        int n = nums.size();
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        int mn = INT_MAX;
-
-        while (j < n) {
-
-            // calculation
-            sum += nums[j];
-
-            if (sum < target) {
-                j++;
-            }
-            else if (sum >= target) {
-
-                while (sum >= target) {
-
-                    mn = min(mn, j - i + 1);
-
-                    sum -= nums[i];
-                    i++;
-                }
-
-                j++;
-            }
-        }
-
-        return (mn == INT_MAX) ? 0 : mn;
-    }
-};
