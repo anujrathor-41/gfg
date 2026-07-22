743b530b2ce5f4903ece8c38bd156cd871ff6805
Anuj Rathor <anujrathor41@gmail.com>
2026-07-22 10:24:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (74.01%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int t[46];
+int solve(int n){
+    if(n<=2) return n;
+
+    if(t[n]!=-1) return t[n];
+
+    int left=solve(n-1);
+    int right=solve(n-2);
+    return t[n]= left+right;
+}
+    int climbStairs(int n) {
+        memset(t,-1,sizeof(t));
+        return solve(n);
+    }
+};
