683677ebbfc7e6483d5615340340e6831622add7
Anuj Rathor <anujrathor41@gmail.com>
2026-07-23 16:05:45 +0530

    Time: 0 ms (100%), Space: 12.8 MB (58.19%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& arr, int k) {
-        
         int n=arr.size();
-        sort(arr.begin(),arr.end());
 
-        for(int i=0;i<n && k>0;i++){
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n && k>0 ;i++){
             if(arr[i]<0){
-                arr[i]=-arr[i];
+                arr[i]= -arr[i];
                 k--;
             }
-        }// neg to pos
+            
+        }// -ve to positive
         int sum=0;
         int mn=INT_MAX;
         for(int i=0;i<n;i++){
-            sum=sum+arr[i];
-            mn=min(abs(arr[i]),mn);// we get min among arr[i]
+            sum += arr[i];
+            mn=min(abs(arr[i]),mn); 
         }
-
-        if(k%2==1){// 6 is min in arr then 
-        // we have to disappear positive 6 and then add negative 6 that's why 2*mn
-            sum -=2*mn;
+        if(k%2==1){//odd hai
+            sum -= 2*mn;
         }
         return sum;
+
     }
 };
