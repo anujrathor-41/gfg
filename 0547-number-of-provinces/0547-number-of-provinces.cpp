38beff5d12fa5c56da8a2d4885949fd19f53092a
Anuj Rathor <anujrathor41@gmail.com>
2026-07-28 16:52:22 +0530

    Time: 5 ms (15.88%), Space: 20.1 MB (42.4%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,13 +1,42 @@
 class Solution {
 public:
 
+// void dfs(int start,vector<vector<int>>& Adj,vector<int>& vis){
+//     vis[start]=1;
+//     for(int i=0;i<Adj[start].size();i++){// or vis.size()
+//         if(vis[i]==0 && Adj[start][i]==1){
+//             dfs(i,Adj,vis);
+//         }
+//     }
+// }
+//     int findCircleNum(vector<vector<int>>& Adj) {
+//         int n=Adj.size();
+//         int cnt=0;
+//         vector<int> vis(n,0);
+//         for(int i=0;i<n;i++){
+//             if(vis[i]==0){
+//                 cnt++;
+//                 dfs(i,Adj,vis);
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
 void dfs(int start,vector<vector<int>>& Adj,vector<int>& vis){
-    vis[start]=1;
+   stack<int> st;
+   st.push(start);
+   while(!st.empty()){
+    int x =st.top();
+    st.pop();
+    vis[x]=1;
+
     for(int i=0;i<vis.size();i++){
-        if(vis[i]==0 && Adj[start][i]==1){
-            dfs(i,Adj,vis);
+        if(Adj[x][i]==1 && vis[i]==0){
+            st.push(i);
         }
     }
+   }
 }
     int findCircleNum(vector<vector<int>>& Adj) {
         int n=Adj.size();
