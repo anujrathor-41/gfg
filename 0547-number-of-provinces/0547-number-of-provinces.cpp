6dc8751d5f07b2d22e524cf5f58bd243a9c6b59c
Anuj Rathor <anujrathor41@gmail.com>
2026-07-29 23:24:47 +0530

    Time: 5 ms (15.83%), Space: 20.2 MB (40.65%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -23,19 +23,49 @@ public:
 //     }
 // };
 
-void dfs(int start,vector<vector<int>>& Adj,vector<int>& vis){
-   stack<int> st;
-   st.push(start);
-   while(!st.empty()){
-    int x =st.top();
-    st.pop();
+// void dfs(int start,vector<vector<int>>& Adj,vector<int>& vis){
+//    stack<int> st;
+//    st.push(start);
+//    while(!st.empty()){
+//     int x =st.top();
+//     st.pop();
+//     vis[x]=1;
+
+//     for(int i=0;i<vis.size();i++){
+//         if(Adj[x][i]==1 && vis[i]==0){
+//             st.push(i);
+//         }
+//     }
+//    }
+// }
+//     int findCircleNum(vector<vector<int>>& Adj) {
+//         int n=Adj.size();
+//         int cnt=0;
+//         vector<int> vis(n,0);
+//         for(int i=0;i<n;i++){
+//             if(vis[i]==0){
+//                 cnt++;
+//                 dfs(i,Adj,vis);
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+void bfs(int start,vector<vector<int>>& Adj,vector<int>& vis){
+   queue<int> q;
+   q.push(start);
+   while(!q.empty()){
+    int x=q.front();
+    
     vis[x]=1;
 
     for(int i=0;i<vis.size();i++){
         if(Adj[x][i]==1 && vis[i]==0){
-            st.push(i);
+            q.push(i);
         }
     }
+    q.pop();
    }
 }
     int findCircleNum(vector<vector<int>>& Adj) {
@@ -45,7 +75,7 @@ void dfs(int start,vector<vector<int>>& Adj,vector<int>& vis){
         for(int i=0;i<n;i++){
             if(vis[i]==0){
                 cnt++;
-                dfs(i,Adj,vis);
+                bfs(i,Adj,vis);
             }
         }
         return cnt;
