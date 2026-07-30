6928085744091a7e9d927a23b0f8ae5eb49828e3
Anuj Rathor <anujrathor41@gmail.com>
2026-07-30 19:56:55 +0530

    Time: 17 ms (59.22%), Space: 25.3 MB (58.22%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,71 +1,169 @@
+// class Solution {
+// public:
+
+//     int bfs(vector<vector<int>>& grid, queue<pair<int,int>>& q,
+//             vector<vector<int>>& dist) {
+
+//         int n = grid.size();
+
+//         while (!q.empty()) {
+
+//             int i = q.front().first;
+//             int j = q.front().second;
+//             q.pop();
+
+//             if (i == n - 1 && j == n - 1)
+//                 return dist[i][j];
+
+//             // Up
+//             if (i - 1 >= 0 && grid[i - 1][j] == 0 && dist[i - 1][j] == 0) {
+//                 dist[i - 1][j] = dist[i][j] + 1;
+//                 q.push({i - 1, j});
+//             }
+
+//             // Down
+//             if (i + 1 < n && grid[i + 1][j] == 0 && dist[i + 1][j] == 0) {
+//                 dist[i + 1][j] = dist[i][j] + 1;
+//                 q.push({i + 1, j});
+//             }
+
+//             // Left
+//             if (j - 1 >= 0 && grid[i][j - 1] == 0 && dist[i][j - 1] == 0) {
+//                 dist[i][j - 1] = dist[i][j] + 1;
+//                 q.push({i, j - 1});
+//             }
+
+//             // Right
+//             if (j + 1 < n && grid[i][j + 1] == 0 && dist[i][j + 1] == 0) {
+//                 dist[i][j + 1] = dist[i][j] + 1;
+//                 q.push({i, j + 1});
+//             }
+
+//             // Up-Left
+//             if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1][j - 1] == 0 &&
+//                 dist[i - 1][j - 1] == 0) {
+//                 dist[i - 1][j - 1] = dist[i][j] + 1;
+//                 q.push({i - 1, j - 1});
+//             }
+
+//             // Up-Right
+//             if (i - 1 >= 0 && j + 1 < n && grid[i - 1][j + 1] == 0 &&
+//                 dist[i - 1][j + 1] == 0) {
+//                 dist[i - 1][j + 1] = dist[i][j] + 1;
+//                 q.push({i - 1, j + 1});
+//             }
+
+//             // Down-Left
+//             if (i + 1 < n && j - 1 >= 0 && grid[i + 1][j - 1] == 0 &&
+//                 dist[i + 1][j - 1] == 0) {
+//                 dist[i + 1][j - 1] = dist[i][j] + 1;
+//                 q.push({i + 1, j - 1});
+//             }
+
+//             // Down-Right
+//             if (i + 1 < n && j + 1 < n && grid[i + 1][j + 1] == 0 &&
+//                 dist[i + 1][j + 1] == 0) {
+//                 dist[i + 1][j + 1] = dist[i][j] + 1;
+//                 q.push({i + 1, j + 1});
+//             }
+//         }
+
+//         return -1;
+//     }
+
+//     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+
+//         int n = grid.size();
+
+//         if (grid[0][0] == 1 )
+//             return -1;
+
+//         queue<pair<int,int>> q;
+//         vector<vector<int>> dist(n, vector<int>(n, 0));
+
+//         q.push({0, 0});
+//         dist[0][0] = 1;
+
+//         return bfs(grid, q, dist);
+//     }
+// };
+
 class Solution {
 public:
 
     int bfs(vector<vector<int>>& grid, queue<pair<int,int>>& q,
-            vector<vector<int>>& dist) {
+            vector<vector<int>>& vis) {
 
         int n = grid.size();
+        int dist = 1;
 
         while (!q.empty()) {
 
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
+            int size = q.size();
 
-            if (i == n - 1 && j == n - 1)
-                return dist[i][j];
+            for (int k = 0; k < size; k++) {
 
-            // Up
-            if (i - 1 >= 0 && grid[i - 1][j] == 0 && dist[i - 1][j] == 0) {
-                dist[i - 1][j] = dist[i][j] + 1;
-                q.push({i - 1, j});
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+
+                if (i == n - 1 && j == n - 1)
+                    return dist;
+
+                // Up
+                if (i - 1 >= 0 && grid[i - 1][j] == 0 && vis[i - 1][j] == 0) {
+                    vis[i - 1][j] = 1;
+                    q.push({i - 1, j});
+                }
+
+                // Down
+                if (i + 1 < n && grid[i + 1][j] == 0 && vis[i + 1][j] == 0) {
+                    vis[i + 1][j] = 1;
+                    q.push({i + 1, j});
+                }
+
+                // Left
+                if (j - 1 >= 0 && grid[i][j - 1] == 0 && vis[i][j - 1] == 0) {
+                    vis[i][j - 1] = 1;
+                    q.push({i, j - 1});
+                }
+
+                // Right
+                if (j + 1 < n && grid[i][j + 1] == 0 && vis[i][j + 1] == 0) {
+                    vis[i][j + 1] = 1;
+                    q.push({i, j + 1});
+                }
+
+                // Up-Left
+                if (i - 1 >= 0 && j - 1 >= 0 &&
+                    grid[i - 1][j - 1] == 0 && vis[i - 1][j - 1] == 0) {
+                    vis[i - 1][j - 1] = 1;
+                    q.push({i - 1, j - 1});
+                }
+
+                // Up-Right
+                if (i - 1 >= 0 && j + 1 < n &&
+                    grid[i - 1][j + 1] == 0 && vis[i - 1][j + 1] == 0) {
+                    vis[i - 1][j + 1] = 1;
+                    q.push({i - 1, j + 1});
+                }
+
+                // Down-Left
+                if (i + 1 < n && j - 1 >= 0 &&
+                    grid[i + 1][j - 1] == 0 && vis[i + 1][j - 1] == 0) {
+                    vis[i + 1][j - 1] = 1;
+                    q.push({i + 1, j - 1});
+                }
+
+                // Down-Right
+                if (i + 1 < n && j + 1 < n &&
+                    grid[i + 1][j + 1] == 0 && vis[i + 1][j + 1] == 0) {
+                    vis[i + 1][j + 1] = 1;
+                    q.push({i + 1, j + 1});
+                }
             }
 
-            // Down
-            if (i + 1 < n && grid[i + 1][j] == 0 && dist[i + 1][j] == 0) {
-                dist[i + 1][j] = dist[i][j] + 1;
-                q.push({i + 1, j});
-            }
-
-            // Left
-            if (j - 1 >= 0 && grid[i][j - 1] == 0 && dist[i][j - 1] == 0) {
-                dist[i][j - 1] = dist[i][j] + 1;
-                q.push({i, j - 1});
-            }
-
-            // Right
-            if (j + 1 < n && grid[i][j + 1] == 0 && dist[i][j + 1] == 0) {
-                dist[i][j + 1] = dist[i][j] + 1;
-                q.push({i, j + 1});
-            }
-
-            // Up-Left
-            if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1][j - 1] == 0 &&
-                dist[i - 1][j - 1] == 0) {
-                dist[i - 1][j - 1] = dist[i][j] + 1;
-                q.push({i - 1, j - 1});
-            }
-
-            // Up-Right
-            if (i - 1 >= 0 && j + 1 < n && grid[i - 1][j + 1] == 0 &&
-                dist[i - 1][j + 1] == 0) {
-                dist[i - 1][j + 1] = dist[i][j] + 1;
-                q.push({i - 1, j + 1});
-            }
-
-            // Down-Left
-            if (i + 1 < n && j - 1 >= 0 && grid[i + 1][j - 1] == 0 &&
-                dist[i + 1][j - 1] == 0) {
-                dist[i + 1][j - 1] = dist[i][j] + 1;
-                q.push({i + 1, j - 1});
-            }
-
-            // Down-Right
-            if (i + 1 < n && j + 1 < n && grid[i + 1][j + 1] == 0 &&
-                dist[i + 1][j + 1] == 0) {
-                dist[i + 1][j + 1] = dist[i][j] + 1;
-                q.push({i + 1, j + 1});
-            }
+            dist++;
         }
 
         return -1;
@@ -75,15 +173,15 @@ public:
 
         int n = grid.size();
 
-        if (grid[0][0] == 1 )
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
             return -1;
 
         queue<pair<int,int>> q;
-        vector<vector<int>> dist(n, vector<int>(n, 0));
+        vector<vector<int>> vis(n, vector<int>(n, 0));
 
         q.push({0, 0});
-        dist[0][0] = 1;
+        vis[0][0] = 1;
 
-        return bfs(grid, q, dist);
+        return bfs(grid, q, vis);
     }
 };
