130e45a51ecf338bceb7d734c4dde289a28902cb
Anuj Rathor <anujrathor41@gmail.com>
2026-07-30 20:29:43 +0530

    Time: 3 ms (26.06%), Space: 16.9 MB (45.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -83,73 +83,161 @@
 //     }
 // };
 
+// class Solution {
+// public:
+
+//     int bfsLevel(vector<vector<int>>& Adj,queue<pair<int,int>> q,int freshCnt){
+//         int n=Adj.size();
+//         int m=Adj[0].size();
+
+//         int time=0;
+//         //up down left right when Adj[i][j]==1 and rott the shell and dec freshcnt and call bfs means push in queue
+//         while(!q.empty() && freshCnt>0){
+//             int size=q.size();
+//             for(int k=0;k<size;k++){
+//                 int i=q.front().first;// pair ka first
+//                 int j=q.front().second;// pair ka secod
+//                 q.pop();
+
+//                 // up
+//                 if(i-1>=0 && Adj[i-1][j]==1){
+//                     Adj[i-1][j]=2;
+//                     freshCnt--;
+//                     q.push({i-1,j});
+//                 }
+//                 // down
+//                 if(i+1<n && Adj[i+1][j]==1){
+//                     Adj[i+1][j]=2;
+//                     freshCnt--;
+//                     q.push({i+1,j});
+//                 }
+//                 if(j-1>=0 && Adj[i][j-1]==1){
+//                     Adj[i][j-1]=2;
+//                     freshCnt--;
+//                     q.push({i,j-1});
+//                 }
+//                 if(j+1<m && Adj[i][j+1]==1){
+//                     Adj[i][j+1]=2;
+//                     freshCnt--;
+//                     q.push({i,j+1});
+//                 }
+
+//             }
+//             time++;
+//         }
+//         if(freshCnt>0) return -1;
+//         return time;
+        
+
+//     }
+
+
+//     int orangesRotting(vector<vector<int>>& Adj) {
+
+//         int n=Adj.size();
+//         int m=Adj[0].size();
+
+//         int freshCnt=0;
+//         queue<pair<int,int>> q;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(Adj[i][j]==2){
+//                     q.push({i,j});
+//                 }
+//                 if(Adj[i][j]==1){
+//                     freshCnt++;
+//                 }
+//             }
+//         }
+//         return bfsLevel(Adj,q,freshCnt);// while + size +for
+//     }
+// };
 class Solution {
 public:
 
-    int bfsLevel(vector<vector<int>>& Adj,queue<pair<int,int>> q,int freshCnt){
-        int n=Adj.size();
-        int m=Adj[0].size();
+    int bfsLevel(vector<vector<int>>& Adj,
+                 queue<pair<int,int>>& q,
+                 vector<vector<int>>& vis,
+                 int freshCnt) {
 
-        int time=0;
-        //up down left right when Adj[i][j]==1 and rott the shell and dec freshcnt and call bfs means push in queue
-        while(!q.empty() && freshCnt>0){
-            int size=q.size();
-            for(int k=0;k<size;k++){
-                int i=q.front().first;// pair ka first
-                int j=q.front().second;// pair ka secod
+        int n = Adj.size();
+        int m = Adj[0].size();
+
+        int time = 0;
+
+        while (!q.empty() && freshCnt > 0) {
+
+            int size = q.size();
+
+            for (int k = 0; k < size; k++) {
+
+                int i = q.front().first;
+                int j = q.front().second;
                 q.pop();
 
-                // up
-                if(i-1>=0 && Adj[i-1][j]==1){
-                    Adj[i-1][j]=2;
+                // Up
+                if (i - 1 >= 0 && Adj[i - 1][j] == 1 && vis[i - 1][j] == 0) {
+                    vis[i - 1][j] = 1;
                     freshCnt--;
-                    q.push({i-1,j});
-                }
-                // down
-                if(i+1<n && Adj[i+1][j]==1){
-                    Adj[i+1][j]=2;
-                    freshCnt--;
-                    q.push({i+1,j});
-                }
-                if(j-1>=0 && Adj[i][j-1]==1){
-                    Adj[i][j-1]=2;
-                    freshCnt--;
-                    q.push({i,j-1});
-                }
-                if(j+1<m && Adj[i][j+1]==1){
-                    Adj[i][j+1]=2;
-                    freshCnt--;
-                    q.push({i,j+1});
+                    q.push({i - 1, j});
                 }
 
+                // Down
+                if (i + 1 < n && Adj[i + 1][j] == 1 && vis[i + 1][j] == 0) {
+                    vis[i + 1][j] = 1;
+                    freshCnt--;
+                    q.push({i + 1, j});
+                }
+
+                // Left
+                if (j - 1 >= 0 && Adj[i][j - 1] == 1 && vis[i][j - 1] == 0) {
+                    vis[i][j - 1] = 1;
+                    freshCnt--;
+                    q.push({i, j - 1});
+                }
+
+                // Right
+                if (j + 1 < m && Adj[i][j + 1] == 1 && vis[i][j + 1] == 0) {
+                    vis[i][j + 1] = 1;
+                    freshCnt--;
+                    q.push({i, j + 1});
+                }
             }
+
             time++;
         }
-        if(freshCnt>0) return -1;
+
+        if (freshCnt > 0)
+            return -1;
+
         return time;
-        
-
     }
-
 
     int orangesRotting(vector<vector<int>>& Adj) {
 
-        int n=Adj.size();
-        int m=Adj[0].size();
+        int n = Adj.size();
+        int m = Adj[0].size();
 
-        int freshCnt=0;
         queue<pair<int,int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(Adj[i][j]==2){
-                    q.push({i,j});
+        int freshCnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (Adj[i][j] == 2) {
+                    q.push({i, j});
+                    vis[i][j] = 1;
                 }
-                if(Adj[i][j]==1){
+
+                if (Adj[i][j] == 1) {
                     freshCnt++;
                 }
             }
         }
-        return bfsLevel(Adj,q,freshCnt);// while + size +for
+
+        return bfsLevel(Adj, q, vis, freshCnt);
     }
 };
