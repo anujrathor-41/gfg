c36b2252b40fe14c0420adc1388b4a4401ea16e5
Anuj Rathor <anujrathor41@gmail.com>
2026-08-01 10:22:42 +0530

    Time: 0 ms (100%), Space: 18.3 MB (31.22%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,44 +1,64 @@
 class Solution {
 public:
 
-    void dfs(int i,int j,vector<vector<int>>& Adj,int oldColor,int newColor){
-    Adj[i][j] =newColor;// curr shell ko change to new color
+    void bfs(vector<vector<int>>& Adj,
+             queue<pair<int,int>>& q,
+             int oldColor,
+             int newColor) {
 
-    int n=Adj.size();
-         int m=Adj[0].size();
-    
+        int n = Adj.size();
+        int m = Adj[0].size();
 
-    // up
-    if(i-1>=0 &&  Adj[i-1][j]==oldColor){// vis contaon integer.
-        dfs(i-1,j,Adj,oldColor,newColor);
+        while (!q.empty()) {
+
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            // Up
+            if (i - 1 >= 0 && Adj[i - 1][j] == oldColor) {
+                Adj[i - 1][j] = newColor;
+                q.push({i - 1, j});
+            }
+
+            // Down
+            if (i + 1 < n && Adj[i + 1][j] == oldColor) {
+                Adj[i + 1][j] = newColor;
+                q.push({i + 1, j});
+            }
+
+            // Left
+            if (j - 1 >= 0 && Adj[i][j - 1] == oldColor) {
+                Adj[i][j - 1] = newColor;
+                q.push({i, j - 1});
+            }
+
+            // Right
+            if (j + 1 < m && Adj[i][j + 1] == oldColor) {
+                Adj[i][j + 1] = newColor;
+                q.push({i, j + 1});
+            }
+        }
     }
-    // down
-    if(i+1<n &&  Adj[i+1][j]==oldColor){
-        dfs(i+1,j,Adj,oldColor,newColor);
-    }
-    // left
-    if(j-1>=0 &&  Adj[i][j-1]==oldColor){
-        dfs(i,j-1,Adj,oldColor,newColor);
-    }
-    // right
-    if(j+1< m &&  Adj[i][j+1]==oldColor){
-        dfs(i,j+1,Adj,oldColor,newColor);
-    }
-    
 
-}
-    vector<vector<int>> floodFill(vector<vector<int>>& Adj, int sr, int sc, int newColor) {
-        int n=Adj.size();
-         int m=Adj[0].size();
+    vector<vector<int>> floodFill(vector<vector<int>>& Adj,
+                                  int sr,
+                                  int sc,
+                                  int newColor) {
 
-       
-        int oldColor=Adj[sr][sc];//condtion 2
-       if(oldColor==newColor) return Adj;// condtion 1
+        int oldColor = Adj[sr][sc];
 
-       dfs(sr,sc,Adj,oldColor,newColor);
-       
-        
-       
+        if (oldColor == newColor)
+            return Adj;
+
+        queue<pair<int,int>> q;
+        q.push({sr, sc});
+
+        // Mark the starting cell as visited
+        Adj[sr][sc] = newColor;
+
+        bfs(Adj, q, oldColor, newColor);
+
         return Adj;
     }
 };
