ae014296083b9f4201d276f9fe7e5034a51f3be2
Anuj Rathor <anujrathor41@gmail.com>
2026-08-02 01:54:17 +0530

    Time: 3 ms (52.22%), Space: 11.9 MB (29.83%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+
+        for(int i=0;i<magazine.size();i++){
+            mp[magazine[i]-'a']++;
+        }
+        for(int i=0;i<ransomNote.size();i++){
+            mp[ransomNote[i]-'a']--;
+            if(mp[ransomNote[i]-'a']<0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
