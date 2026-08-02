84e4f118c8391d1b887c6e3706011762f08eafc5
Anuj Rathor <anujrathor41@gmail.com>
2026-08-02 17:32:08 +0530

    Time: 9 ms (48.56%), Space: 31.9 MB (65.14%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+
+     void bfs(vector<vector<int>>& Adj,vector<vector<int>> &vis,queue<pair<int,int>> &q){
+        int n=Adj.size();
+        int m=Adj[0].size();
+
+        while(!q.empty()){
+            int i=q.front().first;
+            int j=q.front().second;
+
+            q.pop();
+            
+            // up
+            if(i-1>=0 && vis[i-1][j]==0 && Adj[i-1][j]==1){
+                vis[i-1][j]=1;
+                q.push({i-1,j});
+            }
+            // down
+            if(i+1<n && vis[i+1][j]==0 && Adj[i+1][j]==1){
+                vis[i+1][j]=1;
+                q.push({i+1,j});
+            }
+            // left
+            if(j-1>=0 && vis[i][j-1]==0 && Adj[i][j-1]==1){
+                vis[i][j-1]=1;
+                q.push({i,j-1});
+            }
+            // right
+            if(j+1<m && vis[i][j+1]==0 && Adj[i][j+1]==1){
+                vis[i][j+1]=1;
+                q.push({i,j+1});
+            }
+
+        }
+    }
+
+    int numEnclaves(vector<vector<int>>& Adj) {
+
+         int n=Adj.size();
+        int m=Adj[0].size();
+        int cnt=0;
+
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<int,int>> q;
+
+        // first row
+        for(int j=0;j<m;j++){
+           if(Adj[0][j]==1  && vis[0][j]==0){
+            vis[0][j]=1;
+            q.push({0,j});
+            bfs(Adj,vis,q);
+           }
+        }
+        // last row
+        for(int j=0;j<m;j++){
+           if(Adj[n-1][j]==1  && vis[n-1][j]==0){
+            vis[n-1][j]=1;
+            q.push({n-1,j});
+            bfs(Adj,vis,q);
+           }
+        }
+        // first col
+        for(int i=0;i<n;i++){
+            if(Adj[i][0]==1 && vis[i][0]==0){
+                vis[i][0]=1;
+                q.push({i,0});
+                bfs(Adj,vis,q);
+            }
+        }
+        // last col
+        for(int i=0;i<n;i++){
+            if(Adj[i][m-1]==1 && vis[i][m-1]==0){
+                vis[i][m-1]=1;
+                q.push({i,m-1});
+                bfs(Adj,vis,q);
+            }
+        }// yaha tak safe hai boundary condtion
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(Adj[i][j]==1 && vis[i][j]==0){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+        
+    }
+};
