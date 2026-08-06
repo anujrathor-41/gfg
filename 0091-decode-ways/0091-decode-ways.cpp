531c97324da36237e1918a34c942dccbd82b3e6e
Anuj Rathor <anujrathor41@gmail.com>
2026-08-06 12:39:41 +0530

    Time: 2 ms (21.99%), Space: 8.4 MB (92.4%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int t[101];
+
+    int solve(string &s, int n) {
+
+        if (n == 0)
+            return 1;
+
+        int idx = s.size() - n;
+
+        if (s[idx] == '0')
+            return 0;
+
+        if (t[n] != -1)
+            return t[n];
+
+        // Take one digit
+        int one = solve(s, n - 1);
+
+        // Take two digits
+        int two = 0;
+        if (n >= 2) {
+            int num = (s[idx] - '0') * 10 + (s[idx + 1] - '0');
+
+            if (num >= 10 && num <= 26)
+                two = solve(s, n - 2);
+        }
+
+        return t[n] = one + two;
+    }
+
+    int numDecodings(string s) {
+        memset(t, -1, sizeof(t));
+        return solve(s, s.size());
+    }
+};
