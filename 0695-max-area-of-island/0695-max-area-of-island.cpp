16290042f309c613bb501e2f379ce46ebf85b4d3
Anuj Rathor <anujrathor41@gmail.com>
2026-08-08 22:24:46 +0530

    Time: 0 ms (100%), Space: 29.1 MB (29.21%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,86 +1,47 @@
-// class Solution {
-// public:
-//     void dfs(int i,int j,vector<vector<int>>& Adj,vector<vector<int>>& vis){
-//     vis[i][j] =1;
-
-//     int n=Adj.size();
-//          int m=Adj[0].size();
-    
-//     int area=1;
-
-//     // up
-//     if(i-1>=0 && vis[i-1][j]==0 && Adj[i-1][j]==1){// vis contaon integer.
-//         area +=dfs(i-1,j,Adj,vis);
-//     }
-//     // down
-//     if(i+1<n && vis[i+1][j]==0 && Adj[i+1][j]==1){
-//         area +=dfs(i+1,j,Adj,vis);
-//     }
-//     // left
-//     if(j-1>=0 && vis[i][j-1]==0 && Adj[i][j-1]==1){
-//         area +=dfs(i,j-1,Adj,vis);
-//     }
-//     // right
-//     if(j+1< m && vis[i][j+1]==0 && Adj[i][j+1]==1){
-//         area +=dfs(i,j+1,Adj,vis);
-//     }
-
-// }
-//     int maxAreaOfIsland(vector<vector<int>>& grid) {
-//         int n=Adj.size();
-//          int m=Adj[0].size();
-
-//         int cnt=0;
-        
-//         vector<vector<int>> vis(n,vector<int>(m,0));
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<m;j++){
-//                  if(vis[i][j] == 0 && Adj[i][j] ==1){
-              
-//                     int area=dfs(i,j,Adj,vis);
-//                     mx=max(mx,area);
-//                     }
-
-//             }
-           
-//         }
-//         return area;
-//     }
-// };
-
 class Solution {
 public:
-
-    void dfs(int i, int j, vector<vector<int>>& grid,
-             vector<vector<int>>& vis, int& area) {
+    void bfs(vector<vector<int>>& grid, vector<vector<int>>& vis,queue<pair<int,int>> &q  ,
+             int& area) {
 
         int n = grid.size();
         int m = grid[0].size();
 
-        vis[i][j] = 1;
+        while (!q.empty()) {
+           
+            
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
 
-        // count current land cell
-        area++;
+                area++;
 
-        // up
-        if(i - 1 >= 0 && vis[i-1][j] == 0 && grid[i-1][j] == 1) {
-            dfs(i-1, j, grid, vis, area);
+                // up
+                if (i - 1 >= 0 && vis[i - 1][j] == 0 && grid[i - 1][j] == 1) {
+                    q.push({i-1,j});
+                    vis[i-1][j]=1;
+                }
+
+                // down
+                if (i + 1 < n && vis[i + 1][j] == 0 && grid[i + 1][j] == 1) {
+                     q.push({i+1,j});
+                    vis[i+1][j]=1;
+                }
+
+                // left
+                if (j - 1 >= 0 && vis[i][j - 1] == 0 && grid[i][j - 1] == 1) {
+                     q.push({i,j-1});
+                    vis[i][j-1]=1;
+                }
+
+                // right
+                if (j + 1 < m && vis[i][j + 1] == 0 && grid[i][j + 1] == 1) {
+                    q.push({i,j+1});
+                    vis[i][j+1]=1;
+                }
+            
         }
 
-        // down
-        if(i + 1 < n && vis[i+1][j] == 0 && grid[i+1][j] == 1) {
-            dfs(i+1, j, grid, vis, area);
-        }
-
-        // left
-        if(j - 1 >= 0 && vis[i][j-1] == 0 && grid[i][j-1] == 1) {
-            dfs(i, j-1, grid, vis, area);
-        }
-
-        // right
-        if(j + 1 < m && vis[i][j+1] == 0 && grid[i][j+1] == 1) {
-            dfs(i, j+1, grid, vis, area);
-        }
+        
     }
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -91,16 +52,17 @@ public:
         vector<vector<int>> vis(n, vector<int>(m, 0));
 
         int maxi = 0;
+        queue<pair<int,int>> q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-
-                if(grid[i][j] == 1 && vis[i][j] == 0) {
+                if (grid[i][j] == 1 && vis[i][j] == 0) {
 
                     // new island, so start area from 0
                     int area = 0;
-
-                    dfs(i, j, grid, vis, area);
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                    bfs(grid, vis,q, area);
 
                     maxi = max(maxi, area);
                 }
