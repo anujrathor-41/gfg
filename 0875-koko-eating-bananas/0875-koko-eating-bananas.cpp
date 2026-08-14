5472a47deb0382e2cf8b7f2cfb46fce65b551a0c
Anuj Rathor <anujrathor41@gmail.com>
2026-08-14 15:21:21 +0530

    Time: 32 ms (5.05%), Space: 23 MB (55.51%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& arr, int hr) {
+        int e= *max_element(arr.begin(),arr.end());
+        int s=1;
+       
+        int ans=0;
+        while(s<=e){
+            int mid=(s+e)/2;
+            long long curr=0;
+            for(int i=0;i<arr.size();i++){
+                 curr=curr+ ceil((double) arr[i]/mid);
+            }
+            if(curr<=hr){
+                ans=mid;
+                e=mid-1;
+            }else{
+                s=mid+1;
+            }
+        }
+        return ans;
+    }
+};
