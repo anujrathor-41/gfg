70ed70c79f3525880125dfa17fd702a6b29ac2da
Anuj Rathor <anujrathor41@gmail.com>
2026-08-22 12:30:52 +0530

    Time: 0 ms (100%), Space: 42 MB (42.01%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -4,76 +4,22 @@ public:
         int n=arr.size();
         int i=0;
         int j=0;
-        int mn=INT_MAX;
         int sum=0;
-
+        int mn=INT_MAX;
         while(j<n){
-            // cal
             sum=sum+arr[j];
-            
 
-            if(sum<k){
-                j++;
+            while(sum>=k){
+                mn=min(mn,j-i+1);
+
+                sum=sum-arr[i];
+                i++;
             }
-            else if(sum>=k){
-
-                while(sum>=k){
-                    mn=min(mn,j-i+1);
-
-                    sum =sum-arr[i];
-                    i++;
-                }
-                j++;
-                
-            }
-            
-            
+            j++;
         }
         if(mn==INT_MAX){
             return 0;
-        }else {
-            return mn;
         }
+        return mn;
     }
-};// wrong
-
-
-// class Solution {
-// public:
-//     int minSubArrayLen(int target, vector<int>& nums) {
-
-//         int n = nums.size();
-//         int i = 0;
-//         int j = 0;
-//         int sum = 0;
-//         int mn = INT_MAX;
-
-//         while (j < n) {
-
-//             // calculation
-//             sum += nums[j];
-
-//             if (sum < target) {
-//                 j++;
-//             }
-//             else if (sum >= target) {
-
-//                 while (sum >= target) {
-
-//                     mn = min(mn, j - i + 1);
-
-//                     sum -= nums[i];
-//                     i++;
-//                 }
-
-//                 j++;
-//             }
-//         }
-
-//         if(mn==INT_MAX){
-//             return 0;
-//         }else {
-//             return mn;
-//         }
-//     }
-// };
+};
