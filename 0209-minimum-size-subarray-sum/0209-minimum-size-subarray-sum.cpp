bd650d7a8259a37e80b47757bcf350526b5306c5
Anuj Rathor <anujrathor41@gmail.com>
2026-08-22 12:36:04 +0530

    Time: 0 ms (100%), Space: 41.9 MB (74.46%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,25 +1,65 @@
+// class Solution {
+// public:
+//     int minSubArrayLen(int k, vector<int>& arr) {
+//         int n=arr.size();
+//         int i=0;
+//         int j=0;
+//         int sum=0;
+//         int mn=INT_MAX;
+//         while(j<n){
+//             sum=sum+arr[j];
+
+//             while(sum>=k){
+//                 mn=min(mn,j-i+1);
+
+//                 sum=sum-arr[i];
+//                 i++;
+//             }
+//             j++;
+//         }
+//         if(mn==INT_MAX){
+//             return 0;
+//         }
+//         return mn;
+//     }
+// };
+
 class Solution {
 public:
-    int minSubArrayLen(int k, vector<int>& arr) {
-        int n=arr.size();
-        int i=0;
-        int j=0;
-        int sum=0;
-        int mn=INT_MAX;
-        while(j<n){
-            sum=sum+arr[j];
+    int minSubArrayLen(int target, vector<int>& nums) {
 
-            while(sum>=k){
-                mn=min(mn,j-i+1);
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int mn = INT_MAX;
 
-                sum=sum-arr[i];
-                i++;
+        while (j < n) {
+
+            // calculation
+            sum += nums[j];
+
+            if (sum < target) {
+                j++;
             }
-            j++;
+            else if (sum >= target) {
+
+                while (sum >= target) {
+
+                    mn = min(mn, j - i + 1);
+
+                    sum -= nums[i];
+                    i++;
+                }
+
+                j++;
+            }
         }
+
         if(mn==INT_MAX){
             return 0;
+        }else {
+            return mn;
         }
-        return mn;
     }
 };
