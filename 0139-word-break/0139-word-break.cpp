f90a7b185f9251bd6546f2c652f9e1f450f24c67
Anuj Rathor <anujrathor41@gmail.com>
2026-09-08 13:55:07 +0530

    Time: 14 ms (30.89%), Space: 18.4 MB (16.46%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int t[301];
+    bool solve(string s, vector<string>& word, int idx) {
+        if (idx == s.size()) {
+            return true;
+        }
+        if(t[idx]!=-1) return t[idx];
+        for (int i = idx; i < s.size(); i++) {
+
+            string temp = s.substr(idx, i - idx + 1);
+
+            if (find(word.begin(), word.end(), temp) != word.end()) {
+
+                if (solve(s, word, i + 1))
+                   return  t[idx]= true;
+            }
+        }
+         return t[idx] =false;
+    }
+   
+
+ bool wordBreak(string s, vector<string>& word) {
+    memset(t,-1,sizeof(t));
+    return solve(s, word, 0);
+}
+}
+;
