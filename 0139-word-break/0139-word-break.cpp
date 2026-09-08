237b9c04a9187728887befcec292abb4ba6f45c3
Anuj Rathor <anujrathor41@gmail.com>
2026-09-08 13:56:56 +0530

    Time: 17 ms (16.95%), Space: 18.2 MB (17.05%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,3 +1,32 @@
+// class Solution {
+// public:
+// int t[301];
+//     bool solve(string s, vector<string>& word, int idx) {
+//         if (idx == s.size()) {
+//             return true;
+//         }
+//         if(t[idx]!=-1) return t[idx];
+//         for (int i = idx; i < s.size(); i++) {
+
+//             string temp = s.substr(idx, i - idx + 1);
+
+//             if (find(word.begin(), word.end(), temp) != word.end()) {
+
+//                 if (solve(s, word, i + 1))
+//                    return  t[idx]= true;
+//             }
+//         }
+//          return t[idx] =false;
+//     }
+   
+
+//  bool wordBreak(string s, vector<string>& word) {
+//     memset(t,-1,sizeof(t));
+//     return solve(s, word, 0);
+// }
+// }
+// ;
+
 class Solution {
 public:
 int t[301];
@@ -10,7 +39,7 @@ int t[301];
 
             string temp = s.substr(idx, i - idx + 1);
 
-            if (find(word.begin(), word.end(), temp) != word.end()) {
+            if (count(word.begin(), word.end(), temp) >0) {
 
                 if (solve(s, word, i + 1))
                    return  t[idx]= true;
