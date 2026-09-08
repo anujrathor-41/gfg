ce52c8104931ecf3f2e6fb652e114512452cca0e
Anuj Rathor <anujrathor41@gmail.com>
2026-09-09 00:11:05 +0530

    Time: 19 ms (99.4%), Space: 53.4 MB (83.6%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -111,6 +111,62 @@
 //         return ans; 
 //     } 
 // };
+
+
+// class Solution { 
+// public: 
+ 
+//     bool isPalindrome(string &s, int start, int end) { 
+ 
+//         while(start < end) { 
+ 
+//             if(s[start] != s[end]) 
+//                 return false; 
+ 
+//             start++; 
+//             end--; 
+//         } 
+ 
+//         return true; 
+//     } 
+ 
+//     void solve(string &s, int idx, 
+//                vector<string> temp, 
+//                vector<vector<string>>& ans) { 
+ 
+//         if(idx == s.size()) { 
+//             ans.push_back(temp); 
+//             return; 
+//         } 
+ 
+//         for(int i = idx; i < s.size(); i++) { 
+ 
+//             if(isPalindrome(s, idx, i)) { 
+ 
+//                 string str = s.substr(idx, i - idx + 1); 
+ 
+//                 // Fresh copy for this choice 
+//                 // vector<string> newTemp = temp; 
+ 
+//                 // newTemp.push_back(str); 
+//                 temp.push_back(str);
+//                 solve(s, i + 1, temp, ans); 
+//                 temp.pop_back();
+//             } 
+//         } 
+//     } 
+ 
+//     vector<vector<string>> partition(string s) { 
+ 
+//         vector<vector<string>> ans; 
+//         vector<string> temp; 
+ 
+//         solve(s, 0, temp, ans); 
+ 
+//         return ans; 
+//     } 
+// };
+
 class Solution { 
 public: 
  
@@ -129,7 +185,7 @@ public:
     } 
  
     void solve(string &s, int idx, 
-               vector<string> temp, 
+               vector<string>& temp, 
                vector<vector<string>>& ans) { 
  
         if(idx == s.size()) { 
