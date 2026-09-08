b0b5af978abbbed040e91b387f830c1fb229e19a
Anuj Rathor <anujrathor41@gmail.com>
2026-09-08 13:16:29 +0530

    Time: 1 ms (41.85%), Space: 11.9 MB (11.82%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,52 @@
+// class Solution {
+// public:
+//     void solve(vector<int> arr,vector<int> temp,vector<vector<int>> &res,int idx){
+//         int n=arr.size();
+//         if(idx==n) {
+//             res.push_back(temp);
+//             return ;
+//         }
+
+//         for(int i=idx;i<n;i++){
+//             swap(arr[idx],arr[i]);
+//             temp.push_back(arr[idx]);
+//             solve(arr,temp,res,idx+1);
+//             temp.pop_back();
+            
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int>& arr) {
+//         vector<int> temp;
+//         vector<vector<int>> res;
+//         solve(arr,temp,res,0);
+//         return res;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void solve(vector<int> arr,vector<int> temp,vector<vector<int>> &res,int idx){
-        int n=arr.size();
-        if(idx==n) {
-            res.push_back(temp);
-            return ;
+    void solve(vector<int> arr, vector<vector<int>>& res, int idx) {
+        int n = arr.size();
+
+        if (idx == n) {
+            res.push_back(arr);
+            return;
         }
 
-        for(int i=idx;i<n;i++){
-            swap(arr[idx],arr[i]);
-            temp.push_back(arr[idx]);
-            solve(arr,temp,res,idx+1);
-            temp.pop_back();
-            
+        for (int i = idx; i < n; i++) {
+            swap(arr[idx], arr[i]);
+
+            solve(arr, res, idx + 1);
         }
     }
+
     vector<vector<int>> permute(vector<int>& arr) {
-        vector<int> temp;
         vector<vector<int>> res;
-        solve(arr,temp,res,0);
+
+        solve(arr, res, 0);
+
         return res;
     }
 };
