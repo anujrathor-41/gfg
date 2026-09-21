c420683f1b3c35302c206e5e65fdf1bfded98346
Anuj Rathor <anujrathor41@gmail.com>
2026-09-22 00:58:51 +0530

    Time: 276 ms (53.08%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -3,6 +3,6 @@ select a.machine_id ,round(avg(b.timestamp-a.timestamp),3) as processing_time
 from Activity as a
 join 
 Activity as b
-on a.machine_id = b.machine_id and a.process_id=b.process_id
-where a.activity_type='start' and b.activity_type='end'
+on a.machine_id = b.machine_id and a.process_id=b.process_id # kyu ki only machine_id ya process id se kamm nahi chalega
+where a.activity_type='start' and b.activity_type='end' # ye karne ke baad avg niklega
 group by a.machine_id
