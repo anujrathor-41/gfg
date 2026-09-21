3466451c75af27dd3b01542dcb9e71a8489e1fa8
Anuj Rathor <anujrathor41@gmail.com>
2026-09-22 00:09:50 +0530

    Time: 1372 ms (82.67%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,3 +1,11 @@
 # Write your MySQL query statement below
-select customer_id,count(Visits.visit_id) as count_no_trans from Visits left join Transactions 
-on Visits.visit_id= Transactions.visit_id where Transactions.transaction_id is null group by customer_id
+select customer_id,count(Visits.visit_id) as count_no_trans 
+from Visits 
+left join 
+Transactions 
+on Visits.visit_id= Transactions.visit_id
+ where Transactions.transaction_id is null # without make any transaction
+ group by customer_id     # number of time what means that is cutomer_id
+ #customer id group ho gai -> 
+--  is null ke baad we have -> 30,96,54,54
+--  now group them kinko cutomer id ko
