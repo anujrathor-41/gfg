cd98be2def74860fa195ada41da05fdf854bf040
Anuj Rathor <anujrathor41@gmail.com>
2026-09-24 09:54:20 +0530

    Time: 134 ms (22.53%), Space: 7.8 MB (37.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
+    int t[31];
+
+    int solve(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (t[n] != -1)
+            return t[n];
+
+        return t[n] = fib(n - 1) + fib(n - 2);
+    }
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        
-        return fib(n-1) + fib(n-2);
+        memset(t, -1, sizeof(t));
+        return solve(n);
     }
 };
